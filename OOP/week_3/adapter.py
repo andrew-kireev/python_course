2060e5c5f40a1cd0b9cf6b330eb38e41d1a3f720
Andrew <a-kireev1989@mail.ru>
2020-05-01 23:55:42 +0300

    add adapter.py

---
+++ OOP/week_3/adapter.py
@@ -0,0 +1,60 @@
+class Light:
+    def __init__(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+        self.lights = []
+        self.obstacles = []
+
+    def set_dim(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+
+    def set_lights(self, lights):
+        self.lights = lights
+        self.generate_lights()
+
+    def set_obstacles(self, obstacles):
+        self.obstacles = obstacles
+        self.generate_lights()
+
+    def generate_lights(self):
+        return self.grid.copy()
+
+class System:
+    def __init__(self):
+        self.map = self.grid = [[0 for i in range(30)] for _ in range(20)]
+        self.map[5][7] = 1  # Источники света
+        self.map[5][2] = -1  # Стены
+
+    def get_lightening(self, light_mapper):
+        self.lightmap = light_mapper.lighten(self.map)
+
+
+class MappingAdapter:
+    def __init__(self, adaptee):
+        self.adaptee = adaptee
+
+    def lighten(self, grid):
+        dim = (len(grid[0]), len(grid))
+        self.adaptee.set_dim(dim)
+        obstacles = []
+        lights = []
+
+        for i in range(0, len(grid[0])):
+            for j in range(0, len(grid)):
+                if  grid[j][i] == 1:
+                    lights.append((i, j))
+                elif  grid[j][i] == -1:
+                    obstacles.append((i, j))
+        self.adaptee.set_lights(lights)
+        self.adaptee.set_obstacles(obstacles)
+        return self.adaptee.grid
+
+
+
+# print([[0 for i in range(5)] for _ in range(2)])
+#
+# system = System()
+# lig = Light((4,5))
+# adapter = MappingAdapter(lig)
+# system.get_lightening(adapter)
