de3f6110ba7f4a6beabb0dbd886af755bba235f1
Andrew <a-kireev1989@mail.ru>
2020-05-17 00:21:51 +0300

    practice in req expression

---
+++ Web_services_on_python/week_1/req_expression/regexp.py
@@ -0,0 +1,15 @@
+def calculate(data, findall):
+    matches = findall(r"([abc])([+-]){0,1}=([abc]){0,1}([+-]{0,1}\d+){0,1}")  # Если придумать хорошую регулярку, будет просто
+    for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
+        # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
+        if s == '+':
+            data[v1] += data.get(v2, 0) + int(n or 0)
+        elif s == '-':
+            data[v1] -= data.get(v2, 0) + int(n or 0)
+        elif s == '':
+            data[v1] = data.get(v2, 0) + int(n or 0)
+
+    return data
+
+
+
